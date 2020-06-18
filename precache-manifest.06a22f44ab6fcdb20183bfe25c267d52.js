7a5b604998aa1a66c36eb45893f47bef63d11ba7
Natalya Myunster <natalyamyunster@yahoo.com>
2020-06-18 10:28:58 +0200

    Updates

---
+++ precache-manifest.06a22f44ab6fcdb20183bfe25c267d52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f587003ec7961879d9a3d460af2d24d8",
+    "url": "/forms-in-react/index.html"
+  },
+  {
+    "revision": "c0ae74549f89695fa58b",
+    "url": "/forms-in-react/static/css/main.7d7793db.chunk.css"
+  },
+  {
+    "revision": "55ee380e9248ef803acf",
+    "url": "/forms-in-react/static/js/2.d42ea233.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/forms-in-react/static/js/2.d42ea233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ae74549f89695fa58b",
+    "url": "/forms-in-react/static/js/main.96b21433.chunk.js"
+  },
+  {
+    "revision": "46b416471ba00dfe6a1f",
+    "url": "/forms-in-react/static/js/runtime-main.ecd7fc8d.js"
+  },
+  {
+    "revision": "1d8f26fa15da64bfcfa8fb7f6298c02e",
+    "url": "/forms-in-react/static/media/edit-color.1d8f26fa.svg"
+  },
+  {
+    "revision": "1cad8695587c9ddd75cdfa3d0a4c50d4",
+    "url": "/forms-in-react/static/media/edit.1cad8695.svg"
+  }
+]);
